e458dd10548283e80eed9787463865ea5b685432
mamat86pn <absyukur118@gmail.com>
2025-08-27 14:59:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSurprise() {
+  const el = document.getElementById("surprise-text");
+  el.style.display = "block";
+
+  // Tambah efek suara (opsional, jika kamu upload file audio)
+  // const audio = new Audio('haha.mp3');
+  // audio.play();
+}
