0c187194b54d7e9f4463f9b9bdcbf2c8626d4de6
mamat86pn <absyukur118@gmail.com>
2025-08-27 15:27:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function showSurprise() {
   const gif = document.getElementById("gif-reaction");
   const sound = document.getElementById("laugh-sound");
 
+  const gambarAwal = document.getElementById("gambar-awal");
+  const gambarKejutan = document.getElementById("gambar-kejutan");
+
   text.style.display = "block";
   gif.style.display = "block";
   sound.play();
+
+  // Sembunyikan gambar awal, tampilkan gambar kejutan
+  gambarAwal.style.display = "none";
+  gambarKejutan.style.display = "block";
 }
